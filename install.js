a93e641b4beb08eec9e041b45946de67f7e88ae3
6Morpheus6 <wanton-decay@gmx.net>
2025-11-17 18:45:37 +0100

    update
    - add bundle ai
    - remove comments

--- install.js
+++ install.js
@@ -1,6 +1,8 @@
 module.exports = {
+  requires: {
+    bundle: "ai"
+  },
   run: [
-    // Edit this step to customize the git repository to use
     {
       method: "shell.run",
       params: {
@@ -9,7 +11,6 @@ module.exports = {
         ]
       }
     },
-    // Edit this step with your custom install commands
     {
       when: "{{gpu==='nvidia'}}",
       method: "shell.run",
@@ -19,24 +20,22 @@ module.exports = {
         message: "conda install -y anaconda::cudnn --update-deps --force-reinstall"
       }
     },
-    // Delete this step if your project does not use torch
     {
       method: "script.start",
       params: {
         uri: "torch.js",
         params: {
-          venv: "env",                // Edit this to customize the venv folder path
-          path: "app",                // Edit this to customize the path to start the shell from
-          // xformers: true   // uncomment this line if your project requires xformers
+          venv: "env",
+          path: "app",
+          // xformers: true
         }
       }
     },
-    // Edit this step with your custom install commands
     {
       method: "shell.run",
       params: {
-        venv: "env",                // Edit this to customize the venv folder path
-        path: "app",                // Edit this to customize the path to start the shell from
+        venv: "env",
+        path: "app",
         message: [
           "uv pip install gradio devicetorch",
           "uv pip install -r requirements.txt",
